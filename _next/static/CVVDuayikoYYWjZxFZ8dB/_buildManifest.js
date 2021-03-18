6014099ae39db1e5e74b95e15b82561ce96db4b8
neko2x <80758396+neko2x@users.noreply.github.com>
2021-03-18 15:15:22 +0000

    Deploying to gh-pages from  @ 0699816ae6b22881309a66b2c6b9beec35aad3af 🚀

--- _next/static/CVVDuayikoYYWjZxFZ8dB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F3a476af95eac8a39ab2d.css","static\u002Fchunks\u002Fpages\u002Findex-48fdeeaf2847ce4d0323.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-96553f0e25546477ef0b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
